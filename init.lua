6f25e6dc3ad6cbe8ddd2ed28ed7217d2cd93dcc8
Lucas Santos <lsantos@theaiinstitute.com>
2024-03-01 09:25:24 -0500

    changed defaults

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ if not vim.loop.fs_stat(lazypath) then
   require("core.bootstrap").lazy(lazypath)
 end
 
-vim.cmd([[autocmd VimEnter * NvimTreeToggle]])
+-- vim.cmd([[autocmd VimEnter * NvimTreeToggle]])
 
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
