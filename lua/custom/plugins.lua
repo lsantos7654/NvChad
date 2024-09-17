e94fe21e9b5b40d5d59ce21d77def00d6882d14e
Lucas Santos <lsantos7654@gmail.com>
2024-09-16 23:30:24 -0400

    telescope + notify

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -20,7 +20,7 @@ local plugins = {
     version = false, -- set this if you want to always pull the latest change
     opts = {
       mappings = {
-        ask = "<A-a>", -- ask
+        ask = "<A-a>",  -- ask
         edit = "<A-e>", -- edit
       },
       hints = { enabled = false },
@@ -34,7 +34,7 @@ local plugins = {
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -164,11 +164,12 @@ local plugins = {
   -- },
   {
     "rcarriga/nvim-notify",
+    dependencies = "nvim-telescope/telescope.nvim",
     config = function(_, opts)
       require("notify").setup {
         timeout = 3000,
         stages = "fade_in_slide_out",
-        render = "default",
+        render = "minimal",
         background_colour = "#000000",
         on_open = function(win)
           vim.api.nvim_win_set_config(win, { zindex = 100 })
